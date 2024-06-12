0e730abb9633d534a45394f0b661753d273ea775
DESKTOP-IE3O7R6\ccloz <claudiaceleste.9415@gmail.com>
2024-06-11 19:38:07 -0500

    Cambios realizados por mi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app_invitacion_boda';
+
+  isMobile: boolean = false;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.checkScreenSize();
+    window.addEventListener('resize', () => this.checkScreenSize());
+  }
+
+  checkScreenSize() {
+    const mediaQuery = window.matchMedia('(max-width: 767px)');
+    this.isMobile = mediaQuery.matches;
+    console.log({ isMobile : this.isMobile });
+  }
+
+
+
 }
