b5911f4ff6d0e5a1b946fb03b8a831c4d02c1e41
DESKTOP-IE3O7R6\ccloz <claudiaceleste.9415@gmail.com>
2024-07-09 20:40:29 -0500

    Cambio de color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,7 +54,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
   redirectToWhatsApp() {
     const phoneNumber = '+51968238218';
-    const message = '¡Feclicidades! Confirmo mi asistencia a la boda';
+    const message = '¡Felicidades! Confirmo mi asistencia a la boda';
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
   }
