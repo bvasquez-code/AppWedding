a3874ccac79c2ea7e6d1dd81509c7dee2db81b6e
DESKTOP-IE3O7R6\ccloz <claudiaceleste.9415@gmail.com>
2024-07-14 20:47:19 -0500

    cambios Braulio en la casa de Claudia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { interval, Subscription } from 'rxjs';
 import { ExcelService } from './service/ExcelService';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -19,10 +20,20 @@ export class AppComponent implements OnInit, OnDestroy {
   public hours: string = "";
   public minutes: string = "";
   public seconds: string = "";
-  jsonData: any;
+  public jsonData: any;
+  private id: string | null = '';
+  private jsonDataDB: any[] = [];
+  public invitationJson : any;
+  public invitado : string = "";
+  public numInvitados : string = "";
 
 
-  constructor(private excelService : ExcelService) {}
+  constructor(
+    private excelService : ExcelService,
+    private route: ActivatedRoute
+  ) {
+
+  }
 
   ngOnDestroy(): void {
     if (this.subscription) {
@@ -37,6 +48,8 @@ export class AppComponent implements OnInit, OnDestroy {
     this.subscription = interval(1000).subscribe(() => {
       this.updateCountdown();
     });
+
+    // this.loadExcelFile();
   }
 
   checkScreenSize() {
@@ -85,13 +98,64 @@ export class AppComponent implements OnInit, OnDestroy {
     }
   }
 
-  // loadExcelFile() {
-  //   this.excelService.readExcelFileFromAssets().subscribe(data => {
-  //     this.jsonData = data;
-  //     console.log(this.jsonData);
-  //   }, error => {
-  //     console.error('Error reading Excel file', error);
-  //   });
-  // }
+  loadExcelFile() {
+    this.excelService.readExcelFileFromAssets().subscribe(data => {
+
+      this.route.queryParamMap.subscribe(params => {
+        this.id = params.get('id');
+      });
+
+      console.log({ id : this.id});
+      this.jsonData = data;
+      this.parceJson(this.jsonData);
+
+      this.customizeInvitation(this.jsonDataDB,this.id);
+      
+    }, error => {
+      console.error('Error reading Excel file', error);
+    });
+  }
+
+  parceJson(jsonData : any){
+
+    jsonData.shift();
+
+    for(let item of jsonData){
+
+      let itemJson = {
+        invitado	: item[0],
+        numInvitados	: item[1],
+        codInvitacion	: item[2],
+        numCelularConfirmacion : item[3]
+      };
+
+      this.jsonDataDB.push(itemJson);
+    }
+
+    console.log({jsonDataDB : this.jsonDataDB});
+  }
+
+  customizeInvitation(jsonDataDB: any[],id : string | null){
+
+    let invitation : any[] = jsonDataDB.filter( e => e.codInvitacion === id);
+
+    // this.invitationJson = invitation;
+
+    if(invitation.length === 1){
+      this.invitado = invitation[0].invitado;
+      this.numInvitados = invitation[0].numInvitados;
+    }
+    if(invitation.length === 2){
+      this.invitado = `${invitation[0].invitado} y ${invitation[1].invitado}`;
+      this.numInvitados = invitation.reduce((acc, item) => acc + item.numInvitados, 0);
+    }
+    if(invitation.length > 2){
+      this.invitado = invitation.reduce((acc, item) => acc + item.invitado, "");
+      this.numInvitados = invitation.reduce((acc, item) => acc + item.numInvitados, 0);
+    }
+
+    console.log({ invitation : invitation });
+
+  }
 
 }
